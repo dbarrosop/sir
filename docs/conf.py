32bf3fdb3e12e8a54bad8fbfcd49bde457bff07d
David Barroso <dbarroso@spotify.com>
2014-12-02 10:15:10 +0100

    Mocking matplotlib as it fails when compiling on RTD

--- docs/conf.py
+++ docs/conf.py
@@ -272,5 +272,5 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['pandas']
+MOCK_MODULES = ['pandas', 'matplotlib']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
