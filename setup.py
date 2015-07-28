0c1d46a06dacd376c729179f3b8f700ce8dc2543
David Barroso <dbarroso@spotify.com>
2015-07-28 12:49:56 +0200

    This should be ready to send it to pip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import uuid
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-version = '0.9'
+version = '0.10'
 
 install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
 reqs = [str(ir.req) for ir in install_reqs]
