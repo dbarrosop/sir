a4e65e51a3de748ef08a58f11ab9eb49629026d0
David Barroso <dbarrosop@dravetech.com>
2015-08-14 12:33:57 +0200

    Bump version, trying to fix Travis-CI & pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import uuid
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-version = '0.13'
+version = '0.14'
 
 install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
 reqs = [str(ir.req) for ir in install_reqs]
