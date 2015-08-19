8e07b658a9e7fbb8655cc8ee71fa6b172297a468
David Barroso <dbarrosop@dravetech.com>
2015-08-19 15:39:17 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import uuid
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
-version = '0.16'
+version = '0.17'
 
 install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
 reqs = [str(ir.req) for ir in install_reqs]
